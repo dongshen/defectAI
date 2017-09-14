d4590d7eeb1894e18724af931e212effe401c2b2
shendong <shendong70@163.com>
2017-09-15 06:27:41 +0800

    add initial repository and commit

--- src/main/java/sdong/defectAI/utils/GitUtils.java
+++ src/main/java/sdong/defectAI/utils/GitUtils.java
@@ -2,12 +2,16 @@ package sdong.defectAI.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -17,28 +21,55 @@ import sdong.defectAI.exception.GitException;
 public class GitUtils {
 	private static final Logger LOG = Logger.getLogger(GitUtils.class);
 
-	public static Repository createNewRepository(String projectPath) throws IOException {
-		// prepare a new folder
-		File localPath = new File(projectPath);
-		if (localPath.exists()) {
-			throw new IOException("Path for:" + projectPath + " already exist.");
+	public final static String GIT = ".git";
+	public final static String DEFAULT_USER = "amdin";
+	public final static String DEFAULT_USER_EMAIL = "shendong70@163.com";
+	
+
+	public static void initialRepository(String projectPath) throws GitException {
+		try {
+			// prepare a new folder
+			File rootDir = new File(projectPath);
+			// initial
+			if (new File(projectPath + File.separator + GIT).exists() == false) {
+				Git.init().setDirectory(rootDir).call();
+			}
+
+		} catch (IllegalStateException | GitAPIException e) {
+			LOG.error(e);
+			throw new GitException(e);
 		}
-
-		// create the directory
-		Repository repository = FileRepositoryBuilder.create(new File(localPath, ".git"));
-		repository.create();
-
-		return repository;
 	}
 
-	public static Repository getRepository(String projectPath) throws IOException {
+	public static Repository createNewRepository(String projectPath) throws GitException {
+		try {
+			// prepare a new folder
+			File localPath = new File(projectPath);
+			if (localPath.exists()) {
+				throw new IOException("Path for:" + projectPath + " already exist.");
+			}
+
+			// create the directory
+			Repository repository = FileRepositoryBuilder.create(new File(localPath, GIT));
+			repository.create();
+
+			return repository;
+		} catch (IOException e) {
+			LOG.error(e);
+			throw new GitException(e);
+		}
+	}
+
+	public static Repository getRepository(String projectPath) throws GitException {
 		FileRepositoryBuilder builder = new FileRepositoryBuilder();
-		try (Repository repository = builder.setGitDir(new File(projectPath, ".git")).readEnvironment().findGitDir()
+		try (Repository repository = builder.setGitDir(new File(projectPath, GIT)).readEnvironment().findGitDir()
 				.build()) {
 			LOG.debug("Get repository: " + repository.getDirectory());
 			return repository;
+		} catch (IOException e) {
+			LOG.error(e);
+			throw new GitException(e);
 		}
-		// return repository;
 	}
 
 	public static Status addAllChange(Repository repository) throws GitException {
@@ -62,7 +93,7 @@ public class GitUtils {
 		return status;
 
 	}
-	
+
 	public static void showStatus(Status status) throws GitException {
 		LOG.debug("Added: " + status.getAdded());
 		LOG.debug("Changed: " + status.getChanged());
@@ -90,26 +121,87 @@ public class GitUtils {
 			LOG.error(e);
 			throw new GitException(e);
 		}
-		
+
 		return status;
 	}
 
-	public static RevCommit gitCommit(Repository repository, String message) throws GitException {
+	public static RevCommit gitCommit(String projectPath, String remark) throws GitException {
 		RevCommit revCommit = null;
 		try {
-			Git git = new Git(repository);
+			Git git = Git.open(new File(projectPath));
+
+			git.add().addFilepattern(".").call();
+			revCommit = git.commit().setCommitter(DEFAULT_USER, DEFAULT_USER_EMAIL).setMessage(remark).call();
+			LOG.debug("Commit id:" + revCommit.getName());
 
 			git.status().call();
-			revCommit = git.commit().setMessage(message).call();
-			LOG.debug("Commit id:"+revCommit.getName());
-
-			 git.status().call();
 
 			git.close();
-		} catch (GitAPIException e) {
+		} catch (GitAPIException | IOException e) {
 			LOG.error(e);
 			throw new GitException(e);
 		}
 		return revCommit;
+	}
+
+	public static String commitToGitRepository(String gitRoot, String remark) throws GitException {
+		try {
+			File rootDir = new File(gitRoot);
+
+			// initial
+			if (new File(gitRoot + File.separator + GIT).exists() == false) {
+				Git.init().setDirectory(rootDir).call();
+			}
+
+			// 打开git仓库
+			Git git = Git.open(rootDir);
+
+			RevCommit revCommit = git.commit().setCommitter(DEFAULT_USER, DEFAULT_USER_EMAIL).setMessage(remark).call();
+
+			git.close();
+
+			return revCommit.getName();
+		} catch (IllegalStateException | GitAPIException | IOException e) {
+			LOG.error(e);
+			throw new GitException(e);
+		}
+	}
+
+	public static List<String> getChangedFiles(String gitRoot) throws GitException {
+		List<String> updateFiles = new ArrayList<String>();
+		try {
+			File rootDir = new File(gitRoot);
+
+			// 打开git仓库
+			Git git = Git.open(rootDir);
+
+			// 判断工作区与暂存区的文件内容是否有变更
+			List<DiffEntry> diffEntries = git.diff().setShowNameAndStatusOnly(true).call();
+			if (diffEntries == null || diffEntries.size() == 0) {
+				throw new GitException("There is no change.");
+			}
+			// 被修改过的文件
+
+			ChangeType changeType;
+			for (DiffEntry entry : diffEntries) {
+				changeType = entry.getChangeType();
+				switch (changeType) {
+				case ADD:
+				case COPY:
+				case RENAME:
+				case MODIFY:
+					updateFiles.add(entry.getNewPath());
+					break;
+				case DELETE:
+					updateFiles.add(entry.getOldPath());
+					break;
+				}
+			}
+
+		} catch (IOException | GitAPIException e) {
+			LOG.error(e);
+			throw new GitException(e);
+		}
+		return updateFiles;
 	}
 }
--- src/test/java/sdong/defectAI/utils/GitUtilsTest.java
+++ src/test/java/sdong/defectAI/utils/GitUtilsTest.java
@@ -2,37 +2,52 @@ package sdong.defectAI.utils;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
+import java.io.File;
+import java.util.List;
 
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.esotericsoftware.minlog.Log;
 
 import sdong.defectAI.exception.GitException;
 
 public class GitUtilsTest {
 
 	@Test
-	public void testCreateNewRepository() {
+	public void testInitialRepository() {
 		try {
-			Repository repository = GitUtils.createNewRepository("D:/temp/gittest");
-			assertEquals("D:\\temp\\gittest", repository.getDirectory().getPath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void testGetRepository() {
-		try {
-			Repository repository = GitUtils.getRepository("D:/temp/gittest");
-			assertEquals("D:\\temp\\gittest\\.git", repository.getDirectory().getPath());
-		} catch (IOException e) {
+			GitUtils.initialRepository("D:/temp/gittest");
+			assertEquals(true, new File("D:/temp/gittest", GitUtils.GIT).exists());
+		} catch (GitException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	@Test
+	public void testCreateNewRepository() {
+		try {
+			Repository repository = GitUtils.createNewRepository("D:/temp/gittest");
+			assertEquals("D:\\temp\\gittest", repository.getDirectory().getPath());
+		} catch (GitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetRepository() {
+		try {
+			Repository repository = GitUtils.getRepository("D:/temp/gittest");
+			assertEquals("D:\\temp\\gittest\\.git", repository.getDirectory().getPath());
+		} catch (GitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void testAddAllChange() {
@@ -41,20 +56,30 @@ public class GitUtilsTest {
 			GitUtils.addAllChange(repository);
 			GitUtils.gitStatus(repository);
 			assertEquals("D:\\temp\\gittest\\.git", repository.getDirectory().getPath());
-		} catch (IOException | GitException e) {
+		} catch (GitException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void testGitCommite() {
 		try {
-			Repository repository = GitUtils.getRepository("D:/temp/gittest");
-			GitUtils.gitCommit(repository, "commit message");
-			GitUtils.gitStatus(repository);
-			assertEquals("D:\\temp\\gittest\\.git", repository.getDirectory().getPath());
-		} catch (IOException | GitException e) {
+			RevCommit rev = GitUtils.gitCommit("D:/temp/gittest", "commit message");
+			Assert.assertNotNull(rev.name());
+		} catch (GitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetChangedFiles() {
+		try {
+			List<String> changes = GitUtils.getChangedFiles("D:/temp/gittest");
+			Log.debug(changes.toString());
+			//assertEquals("D:\\temp\\gittest\\.git", repository.getDirectory().getPath());
+		} catch (GitException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
